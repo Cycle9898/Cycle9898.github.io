132a39a9033b873577ba88a1dc27d3b41107a588
Cycle9898 <cycle9898@gmail.com>
2024-01-19 17:58:34 +0100

    feat: added dynamic date (current year) on Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
 function Footer() {
+    const today: Date = new Date();
+
     return (
         <footer className="footer">
             <div className="footer__container">
-                <h3>Copyright 2024 - Cyril CLEMENT</h3>
+                <h3>Copyright {today.getFullYear()} - Cyril CLEMENT</h3>
             </div>
         </footer>
     );
