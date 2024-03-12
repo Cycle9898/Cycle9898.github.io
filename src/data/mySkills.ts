46e9d02d57e8287da96f7d47aa23b85b3fc93abe
Cycle9898 <cycle9898@gmail.com>
2024-03-12 11:11:27 +0100

    feat: updated my resume + added MariaDB in techno's

--- src/data/mySkills.ts
+++ src/data/mySkills.ts
@@ -6,6 +6,7 @@ import bootstrapLogo from "../assets/images/Skills/bootstrap.png";
 import jestLogo from "../assets/images/Skills/jest.png";
 import nodejsLogo from "../assets/images/Skills/nodejs.png";
 import mongodbLogo from "../assets/images/Skills/mongodb.png";
+import mariabLogo from "../assets/images/Skills/mariadb.png";
 
 import gitLogo from "../assets/images/Skills/git.png";
 import githubLogo from "../assets/images/Skills/github.png";
@@ -17,93 +18,98 @@ import linuxLogo from "../assets/images/Skills/linux.png";
 import windowsLogo from "../assets/images/Skills/windows.png";
 
 export type SkillType = {
-    id?: string;
-    name: string;
-    logo: string;
+	id?: string;
+	name: string;
+	logo: string;
 };
 
 export const technologies: SkillType[] = [
-    {
-        id: "Tech01",
-        name: "TypeScript",
-        logo: typescriptLogo
-    },
-    {
-        id: "Tech02",
-        name: "React",
-        logo: reactLogo
-    },
-    {
-        id: "Tech03",
-        name: "Redux",
-        logo: reduxLogo
-    },
-    {
-        id: "Tech04",
-        name: "Sass",
-        logo: sassLogo
-    },
-    {
-        id: "Tech05",
-        name: "Bootstrap",
-        logo: bootstrapLogo
-    },
-    {
-        id: "Tech06",
-        name: "Jest",
-        logo: jestLogo
-    },
-    {
-        id: "Tech07",
-        name: "Node.JS",
-        logo: nodejsLogo
-    },
-    {
-        id: "Tech08",
-        name: "MongoDB",
-        logo: mongodbLogo
-    }
+	{
+		id: "Tech01",
+		name: "TypeScript",
+		logo: typescriptLogo
+	},
+	{
+		id: "Tech02",
+		name: "React",
+		logo: reactLogo
+	},
+	{
+		id: "Tech03",
+		name: "Redux",
+		logo: reduxLogo
+	},
+	{
+		id: "Tech04",
+		name: "Sass",
+		logo: sassLogo
+	},
+	{
+		id: "Tech05",
+		name: "Bootstrap",
+		logo: bootstrapLogo
+	},
+	{
+		id: "Tech06",
+		name: "Jest",
+		logo: jestLogo
+	},
+	{
+		id: "Tech07",
+		name: "Node.JS",
+		logo: nodejsLogo
+	},
+	{
+		id: "Tech08",
+		name: "MongoDB (No SQL)",
+		logo: mongodbLogo
+	},
+	{
+		id: "Tech09",
+		name: "MariaDB (SQL)",
+		logo: mariabLogo
+	}
 ];
 
 export const tools: SkillType[] = [
-    {
-        id: "Tool01",
-        name: "Git",
-        logo: gitLogo
-    },
-    {
-        id: "Tool02",
-        name: "GitHub",
-        logo: githubLogo
-    },
-    {
-        id: "Tool03",
-        name: "ViteJS",
-        logo: viteLogo
-    },
-    {
-        id: "Tool04",
-        name: "Figma",
-        logo: figmaLogo
-    },
-    {
-        id: "Tool05",
-        name: "Swagger",
-        logo: swaggerLogo
-    },
-    {
-        id: "Tool06",
-        name: "Lighthouse",
-        logo: lighthouseLogo
-    },
-    {
-        id: "Tool07",
-        name: "Linux",
-        logo: linuxLogo
-    },
-    {
-        id: "Tool08",
-        name: "Windows",
-        logo: windowsLogo
-    }
+	{
+		id: "Tool01",
+		name: "Git",
+		logo: gitLogo
+	},
+	{
+		id: "Tool02",
+		name: "GitHub",
+		logo: githubLogo
+	},
+	{
+		id: "Tool03",
+		name: "ViteJS",
+		logo: viteLogo
+	},
+	{
+		id: "Tool04",
+		name: "Figma",
+		logo: figmaLogo
+	},
+	{
+		id: "Tool05",
+		name: "Swagger",
+		logo: swaggerLogo
+	},
+	{
+		id: "Tool06",
+		name: "Lighthouse",
+		logo: lighthouseLogo
+	},
+	{
+		id: "Tool07",
+		name: "Linux",
+		logo: linuxLogo
+	},
+	{
+		id: "Tool08",
+		name: "Windows",
+		logo: windowsLogo
+	}
 ];
